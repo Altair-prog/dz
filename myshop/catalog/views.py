fc5aa0eb920bd5e69cf4572cca1f215d06c646ad
Aleks <rezultat-24@yandex.ru>
2024-08-11 15:10:47 +0300

    Добавлен контакт

---
+++ myshop/catalog/views.py
@@ -0,0 +1,10 @@
+from django.shortcuts import render
+
+def home(request):
+    return render(request, 'catalog/home.html')
+
+def contact(request):
+    return render(request, 'catalog/contact.html')
+from django.shortcuts import render
+
+# Create your views here.
